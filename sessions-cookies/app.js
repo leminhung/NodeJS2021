2959aad83bf766ba1b2d8eae6284bcc9760527cd
le minh hung <leminhhungtabletennis@gmail.com>
2021-11-29 14:26:17 +0700

    Finished understanding sessions, cookie

---
+++ sessions-cookies/app.js
@@ -0,0 +1,78 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const MONGO_URL =
+  "mongodb+srv://leminhunglmh:leminhhung123@cluster0.otp0j.mongodb.net/shop";
+
+let store = new MongoDBStore({
+  uri: MONGO_URL,
+  collection: "sessions",
+});
+
+const errorController = require("./controllers/error");
+
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "My secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log("[err--]", err.message));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    console.log("Connect to server successfully");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ sessions-cookies/controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("61a23d5225824a1ffae2f0a3")
+    .then((user) => {
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      req.session.save((err) => {
+        console.log("[err--]", err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
